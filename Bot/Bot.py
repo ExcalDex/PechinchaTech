9e3a580600cd1ddb5884792c4538e0d54af3a574
DavidMGSNero <davidmaquarteesilva@gmail.com>
2024-08-12 12:14:45 -0300

    new file:   Bot/Bot.py 	new file:   Bot/Mercado_Livre.py 	new file:   Bot/Pichau.py 	new file:   Bot/Terabyte.py 	new file:   Bot/__pycache__/Amazon.cpython-312.pyc 	new file:   Bot/__pycache__/Bot.cpython-311.pyc 	new file:   Bot/__pycache__/Bot.cpython-312.pyc 	new file:   Bot/__pycache__/Kabum.cpython-311.pyc 	new file:   Bot/__pycache__/Kabum.cpython-312.pyc 	new file:   Bot/__pycache__/Kabum_Galery.cpython-312.pyc     new file:   Bot/__pycache__/Kabum_PBP.cpython-312.pyc     new file:   Bot/__pycache__/Mercado_Livre.cpython-312.pyc 	new file:   Bot/__pycache__/Mercado_Livre_Galery.cpython-312.pyc 	new file:   Bot/__pycache__/Mercado_Livre_PBP.cpython-312.pyc 	new file:   Bot/__pycache__/Terabyte.cpython-312.pyc 	new file:   Main.py 	new file:   Model/ManageProduto.py 	new file:   Model/__pycache__/ManageProduto.cpython-311.pyc 	new file:   Model/__pycache__/ManageProduto.cpython-312.pyc 	new file:   __pycache__/ManageProduto.cpython-311.pyc 	new file:   benchmark/CPU_UserBenchmarks.csv 	new file:   benchmark/GPU_UserBenchmarks.csv 	new file:   benchmark/HDD_UserBenchmarks.csv 	new file:   benchmark/RAM_UserBenchmarks.csv 	new file:   benchmark/SSD_UserBenchmarks.csv 	new file:   gpu_amazon.json 	new file:   gpu_kabum.json 	new file:   gpu_kabum_galery.json 	new file:   gpu_mercado_livre.json 	new file:   gpu_mercado_livre_galery.json
    	modified:   Bot/Bot.py

---
+++ Bot/Bot.py
@@ -0,0 +1,11 @@
+import Bot.Kabum_Galery
+import Bot.Kabum_PBP
+import Bot.Mercado_Livre
+import json
+
+def run() -> dict:
+    bot = Bot.Kabum_Galery.Scraper("placa de video")
+    data = bot.get_produtos()
+    with open("gpu_kabum_galery.json", "w") as json_file:
+        json.dump(data, json_file, indent=4)
+    return data
