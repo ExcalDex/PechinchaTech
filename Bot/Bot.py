60c7d5fc3441b8737112c2eef6a672bb0fbbb43f
David Maquarte <162143529+DavidMGSNero@users.noreply.github.com>
2024-08-12 12:19:24 -0300

    Delete Bot directory

--- Bot/Bot.py
+++
@@ -1,9 +0,0 @@
-import Bot.Mercado_Livre
-import json
-
-def run() -> dict:
-    bot = Bot.Kabum_Galery.Scraper("placa de video")
-    data = bot.get_produtos()
-    with open("gpu_kabum_galery.json", "w") as json_file:
-        json.dump(data, json_file, indent=4)
-    return data
