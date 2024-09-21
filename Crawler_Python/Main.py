46a23b9bda060d155fab1df0518377bc0ab6b359
Andre <andredesenvolvimentosis@gmail.com>
2024-09-21 20:08:20 -0300

    Requisições para a API

--- Crawler_Python/Main.py
+++ Crawler_Python/Main.py
@@ -5,54 +5,44 @@ import requests
 from requests.adapters import HTTPAdapter
 
 
-# def create_http_session():
-#     adapter = HTTPAdapter()
-#     session = requests.Session()
-#     session.mount("http://", adapter)
-#     session.mount("https://", adapter)
-#     return session
+def create_http_session():
+    adapter = HTTPAdapter()
+    session = requests.Session()
+    session.mount("http://", adapter)
+    session.mount("https://", adapter)
+    return session
 
 
 if __name__ == "__main__":
     data = Bot.run()  # Dados puros dos produtos
-    new_data = []
-    for i in range(len(data["Nome"])):
-        produto = {"nome": data["Nome"][i], "preco": data["Valor"][i], "link": data["Link"][i], "tipo": data["Tipo"][i], "loja": data["Loja"][i], "link_imagem": data["Imagem"][i]}
-        new_data.append(produto)
     with open("teste.json", "w", encoding="utf-8") as f:
-        f.write(json.dumps(new_data, indent=6))
-    # p_list: list[Produto] = []
-    # for i in range(len(data["Nome"])):
-    #     p_list.append(
-    #         Produto(
-    #             data["Nome"][i],
-    #             data["Tipo"][i],
-    #             data["Valor"][i],
-    #             data["Link"][i],
-    #             data["Loja"][i],
-    #             data["Imagem"][i],  # Só falta o bot do Mercado livre extrair as imagens também
-    #         )
-    #     )
+        f.write(json.dumps(data))
+    p_list: list[Produto] = []
+    for i in range(len(data["Nome"])):
+        p_list.append(
+            Produto(
+                data["Nome"][i],
+                data["Tipo"][i],
+                float(data["Valor"][i]),
+                data["Link"][i],
+                data["Loja"][i],
+                data["Imagem"][i],
+            )
+        )
 
-    # p_list = [p for p in p_list if not p.get_nome().isspace()]
+    p_list = [p for p in p_list if not p.get_nome().isspace()]
 
-    # json_list = []
-    # session = create_http_session()
-    # try:
-    #     for i, p in enumerate(p_list):
-    #         json_obj = p.to_dict()
-    #         if json_obj['idProdutoBase'] != -1:
-    #             json_list.append(json_obj)
-    #         if (i + 1) % 100 == 0 or i == len(p_list) - 1:
-    #             try:
-    #                 response = session.post(
-    #                     "http://localhost:8087/api/produto/batch",
-    #                     data=json.dumps(json_list),
-    #                     headers={"Content-Type": "application/json"}
-    #                 )
-    #                 response.raise_for_status()
-    #             except requests.exceptions.RequestException as e:
-    #                 print(f"Request failed: {e}")
-    #             json_list.clear()  # Libera memória RAM
-    # finally:
-    #     session.close()
+    json_list = []
+    session = create_http_session()
+    try:
+        for p in p_list:
+            json_obj = p.to_dict()
+            if json_obj["idProdutoBase"] != -1:
+                json_list.append(json_obj)
+            session.post(
+                "http://localhost:8087/api/produto/batch",
+                data=json.dumps(json_list),
+                headers={"Content-Type": "application/json"},
+            )
+    finally:
+        session.close()
