ced42d2fe13a83e5ecdbf369653b4d2036b364fc
André Santos <122818731+ExcalDex@users.noreply.github.com>
2024-09-21 20:20:28 -0300

    Delete Site/PechinchaTech directory

--- Site/PechinchaTech/src/app/model/notif.ts
+++
@@ -1,9 +0,0 @@
-export class Notif {
-    id_produto_base: number;
-    id_usuario: number;
-
-    constructor() {
-        this.id_produto_base = 0;
-        this.id_usuario = 0;
-    }
-}
--- Site/PechinchaTech/src/app/model/produto-base.spec.ts
+++
@@ -1,7 +0,0 @@
-import { ProdutoBase } from './produto-base';
-
-describe('ProdutoBase', () => {
-  it('should create an instance', () => {
-    expect(new ProdutoBase()).toBeTruthy();
-  });
-});
