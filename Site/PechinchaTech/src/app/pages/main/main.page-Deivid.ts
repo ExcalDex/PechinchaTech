004f350c630dba4f1a29ce268c61631182cf46d4
DavidMGSNero <davidmaquarteesilva@gmail.com>
2024-10-16 14:02:00 -0300

    Site v0.9.1

---
+++ Site/PechinchaTech/src/app/pages/main/main.page-Deivid.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/model/user';
+import { ProdutoBase } from 'src/app/model/produto-base';
+import { Produto } from 'src/app/model/produto';
+import { Notif } from 'src/app/model/notif';
+import { ProdutoBaseService } from 'src/app/services/produto-base.service'
+import { ProdutoService } from 'src/app/services/produto.service';
+import { NotifService } from 'src/app/services/notif.service';
+import { UserService } from 'src/app/services/user.service';
+import { ToastController } from '@ionic/angular';
+import { NavController } from '@ionic/angular';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.page.html',
+  styleUrls: ['./main.page.scss'],
+})
+export class MainPage implements OnInit {
+
+  userAutenticado: User;
+  produtos: Produto[];
+
+  constructor(public router: Router, private toastController: ToastController, private navController: NavController, private userService: UserService, private produtoBaseService: ProdutoBaseService, private produtoService: ProdutoService, private notifService: NotifService) { 
+    this.userAutenticado = new User();
+    this.produtos = [];
+  }
+
+  ngOnInit() {
+    this.userAutenticado = this.userService.getUserAutenticado();
+  }
+
+  async ionViewWillEnter() {
+    this.carregarListaProdutos();
+  }
+
+  verificaUserAutenticado(): boolean {
+    if (this.userAutenticado.id != null) {
+      return true;
+    }
+    return false;
+  }
+
+  encerrarSessao() {
+    this.userService.encerrarSessao();
+    this.navController.navigateForward('/login');
+  }
+
+  async carregarListaProdutos() {
+    this.produtos =  await this.produtoService.listarProdutos();
+    console.log(this.produtos);
+  }
+
+  formatarPreco(preco: number) {
+    return preco.toFixed(2).replace('.', ',');
+  }
+}
